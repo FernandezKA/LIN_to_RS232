57b71beef15fa05876b3f6157bf826c7c3f5e09f
Kirill Fernandez <fernandes.kir@yandex.ru>
2022-03-24 13:05:42 +0300

    Rewrited protocol from text to numeric

--- Src/prot.c
+++ Src/prot.c
@@ -5,53 +5,77 @@ enum avCommands GetCommand(uint8_t currByte)
     enum avCommands currCommand = none_command;
     switch (currByte)
     {
-    case 'b':
-        currCommand = setBaud;
-        break;
+//    case 'b':
+//        currCommand = setBaud;
+//        break;
 
-    case 'B':
-        currCommand = setBaud;
-        break;
+//    case 'B':
+//        currCommand = setBaud;
+//        break;
 
-    case 'e':
-        currCommand = setCRC;
-        break;
+//    case 'e':
+//        currCommand = setCRC;
+//        break;
 
-    case 'E':
-        currCommand = setCRC;
-        break;
+//    case 'E':
+//        currCommand = setCRC;
+//        break;
 
-    case 'c':
-        currCommand = setFilter;
-        break;
+//    case 'c':
+//        currCommand = setFilter;
+//        break;
 
-    case 'C':
-        currCommand = setFilter;
-        break;
+//    case 'C':
+//        currCommand = setFilter;
+//        break;
 
-    case 'i':
-        currCommand = getInfo;
-        break;
+//    case 'i':
+//        currCommand = getInfo;
+//        break;
 
-    case 'I':
-        currCommand = getInfo;
-        break;
+//    case 'I':
+//        currCommand = getInfo;
+//        break;
 
-    case 's':
-        currCommand = sendSlave;
-        break;
+//    case 's':
+//        currCommand = sendSlave;
+//        break;
 
-    case 'S':
-        currCommand = sendSlave;
-        break;
+//    case 'S':
+//        currCommand = sendSlave;
+//        break;
 
-    case 'm':
-        currCommand = sendMaster;
-        break;
+//    case 'm':
+//        currCommand = sendMaster;
+//        break;
 
-    case 'M':
-        currCommand = sendMaster;
-        break;
+//    case 'M':
+//        currCommand = sendMaster;
+//        break;
+
+			case 0x01:
+				currCommand = getInfo;
+			break;
+			
+			case 0x02:
+				currCommand = setBaud;
+			break;
+			
+			case 0x03: 
+				currCommand = setCRC;
+			break;
+			
+			case 0x04:
+				currCommand = setFilter;
+			break;
+			
+			case 0x05:
+				currCommand = sendSlave;
+			break;
+			
+			case 0x06:
+				currCommand = sendMaster;
+			break;
     }
     return currCommand;
 }
